46340bc6aa0cfed8b409f1e82d8afb6269f17bb5
Henrique Uriel <riikilopes262@hotmail.comgit config --global user.name Henrique>
2021-08-07 00:03:50 -0300

    Exercicios Lista 4 POO

---
+++ Java/AULAS/src/Lista4Class/FuncionarioClass.java
@@ -0,0 +1,45 @@
+package Lista4Class;
+
+public class FuncionarioClass {
+
+	
+	public double salario;
+	public String nome;
+	public String genero;
+	public int idade;
+	public String empresa;
+	
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getEmpresa() {
+		return empresa;
+	}
+	public void setEmpresa(String empresa) {
+		this.empresa = empresa;
+	}
+	
+	
+	
+}
---
+++ Java/AULAS/src/Lista4Main/Funcionario.java
@@ -0,0 +1,27 @@
+package Lista4Main;
+
+import Lista4Class.FuncionarioClass;
+
+public class Funcionario {
+
+	public static void main(String[] args) {
+		
+		FuncionarioClass colaborador = new FuncionarioClass();
+		
+		//declarei (instanciar)
+		colaborador.setNome("Marcos");    
+		colaborador.setIdade(29);
+		colaborador.setGenero("Homem");
+		colaborador.setEmpresa("Bradesco");
+		colaborador.setSalario(2500);
+		//retorno
+		System.out.println("Nome: "+colaborador.getNome());
+		System.out.println("Idade: "+colaborador.getIdade()+" anos");
+		System.out.println("Genero: "+colaborador.getGenero());
+		System.out.println("Empregado da Empresa: "+colaborador.getEmpresa());
+		System.out.println("Salario Atual: R$ "+colaborador.getSalario());
+				
+
+	}
+
+}
