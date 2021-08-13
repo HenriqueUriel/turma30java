c22ab01f2ad83583cf557f9e728c07a0feee4b6f
Henrique Uriel <riikilopes262@hotmail.comgit config --global user.name Henrique>
2021-08-13 17:17:35 -0300

    adicionado forma de pagamento e nota fiscal

--- Java/LOJAPOO/src/entities/Funcoes.java
+++ Java/LOJAPOO/src/entities/Funcoes.java
@@ -1,5 +1,8 @@
 package entities;
 
+import java.text.DecimalFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Scanner;
 
@@ -38,13 +41,21 @@ public class Funcoes {
 	}
 
 	public static void apresentaTabela(List<Produto> lista, List<Produto> carrinho) {
+		DecimalFormat df = new DecimalFormat("#.00");//arredondamento do valor								
+		LocalDateTime agora = LocalDateTime.now();//data/hora atual
+		DateTimeFormatter formatterData = DateTimeFormatter.ofPattern("dd/MM/uuuu");// formatar a data
+		String dataFormatada = formatterData.format(agora);// formatar a hora
+		DateTimeFormatter formatterHora = DateTimeFormatter.ofPattern("HH:mm:ss");
+		String horaFormatada = formatterHora.format(agora);
 		String auxCod;
 		int auxQuant = 0;
 		int pos=-1;
 		char op;
+		int opcaoCompra =0;
 		Scanner ler = new Scanner(System.in);
 		if(entraNosite()) {
 			do {
+				//tabela dos produtos
 				pos = -1;
 				for (Produto item : lista) {
 					System.out.println(item.getCodProduto()+"\t"+item.getPreco()+"\t\t"+item.getEstoque()+"\t\t"+item.getProduto()+((item.getEstoque()>0)?" ":"\tPRODUTO INDISPONIVEL"));
@@ -54,10 +65,9 @@ public class Funcoes {
 				for (Produto escolhido : carrinho) {
 					System.out.println(escolhido.getCodProduto()+"\t"+escolhido.getPreco()+"\t"+escolhido.getEstoque()+"\t"+escolhido.getProduto());
 				}
+				//escolher o produto
 					System.out.print("Digite o codigo do produto: ");
-				auxCod = ler.next().toUpperCase();
-				//pos = lista.indexOf(auxCod);
-				
+				auxCod = ler.next().toUpperCase();	
 				for (int x=0; x<lista.size(); x++ ) {
 					
 					if (lista.get(x).getCodProduto().equals(auxCod)) {
@@ -66,18 +76,19 @@ public class Funcoes {
 					}
 					
 				}
-			
+				//Porduto escolhido e dados do mesmo
 				if (pos>=0) {
 					System.out.println("Produto escolhido: \n");
-					System.out.println("\nCOD\tVALOR\tESTOQUE\tPRODUTO");
+					System.out.println("\nCOD\tVALOR  ESTOQUE\t PRODUTO");
 					System.out.printf("%s\t%.2f\t%d\t%s\n",lista.get(pos).getCodProduto(),lista.get(pos).getPreco(),lista.get(pos).getEstoque(),lista.get(pos).getProduto());
 					System.out.print("\nQuantidade do Produto: ");
 					int auxQtde = ler.nextInt();
+					//validações de quantidade do produto
 					if (auxQtde< 0) {
 						System.out.println("Impossivel realizar, valor negativo!!!");
 					}
 					else if (auxQtde==0) {
-						System.out.println("Impossivel realizar, quantidade zero.");
+						System.out.println("Impossivel realizar, nenhuma quantidade foi escolhida.");
 					}
 					else if (lista.get(pos).getEstoque()==0){
 						System.out.println("Impossivel realizar, produto sem estoque!!!");
@@ -90,13 +101,13 @@ public class Funcoes {
 				}
 				else {
 					System.out.println("Codigo informado não existe!!!");
-				}
-				System.out.print("Continua SIM/NAO [S/N] :");
+				}//continua com a compra
+				System.out.print("\n\nContinua SIM/NAO [S/N] :");
 				op = ler.next().toUpperCase().charAt(0);
 
-			} while (op=='S');
-			System.out.println("Fechamento da Compra");
-			System.out.println("COD\tVALOR\tESTOQUE\tPRODUTO");
+			} while (op=='S');//fechamento compra
+			System.out.println("\n\nFechamento da Compra:");
+			System.out.println("COD\tVALOR  ESTOQUE\t PRODUTO");
 			double auxTotal=0;
 			for (Produto escolhido : carrinho) {
 					for (int x=0; x<lista.size(); x++ ) {
@@ -104,23 +115,71 @@ public class Funcoes {
 						if (lista.get(x).getCodProduto().equals(escolhido.getCodProduto())) {
 							pos = x;
 							break;
-						}
-						
+						}				
 					}
 				System.out.println(escolhido.getCodProduto()+"\t"+escolhido.getPreco()+"\t"+escolhido.getEstoque()+"\t"+escolhido.getProduto());
 				auxTotal = auxTotal + (escolhido.getPreco()*escolhido.getEstoque());
-				lista.get(pos).retiraEstoque(escolhido.getEstoque());				
-			}
-			System.out.println("TOTAL R$: "+auxTotal+"\n\r");	
+				lista.get(pos).retiraEstoque(escolhido.getEstoque());
+				//valor total da compra		
+				System.out.printf("\nVALOR DA COMPRA : %.2f",auxTotal);
+				System.out.println("\n\nFORMAS DE PAGAMENTO : ");
+				System.out.println("Opção [1] - Dinheiro á vista c/ 10% de desconto");
+				System.out.println("Opção [2] - Cartão á vista c/ 10% de acréscimo");
+				System.out.println("Opção [3] - Cartão em 2x c/ 15% de acréscimo");
+				System.out.print("\nDigite a opção de pagamento : ");
+				opcaoCompra = ler.nextInt();
+				System.out.print("\n\n\n");
+				//compra a vista dindin
+				if (opcaoCompra == 1) {
+					System.out.print("------------------------------------------------\n");
+					System.out.print("***** NOTA FISCAL PAULISTA *****\n");
+					System.out.print("------------------------------------------------\n");
+					System.out.println("COD: "+escolhido.getCodProduto()+"  Prod: "+escolhido.getProduto()+"  R$"+escolhido.getPreco()+"  "+escolhido.getEstoque()+"und");
+					System.out.println("Dinheiro à vista");
+					System.out.println("9% de ICMS : R$ "+df.format(auxTotal * 0.09));
+					System.out.println("10% de desconto : R$ "+df.format(auxTotal * 0.1));
+					System.out.println("\nTOTAL DA COMPRA R$ "+df.format(auxTotal * 0.9)+"\n");
+					System.out.print("Data de emissão : "+formatterData.format(agora)+"\tHora : "+formatterHora.format(agora)+"\n");
+					System.out.print("------------------------------------------------\n\n\n");
+				}
+				// a vista cartão
+				if (opcaoCompra == 2) {
+					System.out.print("------------------------------------------------\n");
+					System.out.print("***** NOTA FISCAL PAULISTA *****\n");
+					System.out.print("------------------------------------------------\n");
+					System.out.println("COD: "+escolhido.getCodProduto()+"  Prod: "+escolhido.getProduto()+"  R$"+escolhido.getPreco()+"  "+escolhido.getEstoque()+"und");
+					System.out.println("\n\nCartão à vista");
+					System.out.println("9% de ICMS : R$ "+df.format(auxTotal * 0.09));
+					System.out.println("10% de acréscimo : R$ "+df.format(auxTotal * 0.1));
+					System.out.println("\nTOTAL DA COMPRA R$ "+df.format(auxTotal * 1.1)+"\n");
+					System.out.print("Data de emissão : "+formatterData.format(agora)+"\tHora : "+formatterHora.format(agora)+"\n");
+					System.out.print("------------------------------------------------\n\n\n");
+				}
+				// 2x no cartão
+				if (opcaoCompra == 3) {
+					System.out.print("------------------------------------------------\n");
+					System.out.print("***** NOTA FISCAL PAULISTA *****\n");
+					System.out.print("------------------------------------------------\n");
+					System.out.println("COD: "+escolhido.getCodProduto()+"  Prod: "+escolhido.getProduto()+"  R$"+escolhido.getPreco()+"  "+escolhido.getEstoque()+"und");
+					System.out.println("\n\n2X no Cartão");
+					System.out.println("9% de ICMS : R$ "+df.format(auxTotal * 0.09));
+					System.out.println("15% de acréscimo : R$ "+df.format(auxTotal * 0.15));
+					System.out.println("Valor da parcela : R$ "+df.format((auxTotal * 1.15)/2)+"\n");
+					System.out.println("\nTOTAL DA COMPRA R$ " +df.format(auxTotal * 1.15)+"\n");
+					System.out.print("Data de emissão : "+formatterData.format(agora)+"\tHora : "+formatterHora.format(agora)+"\n");
+					System.out.print("------------------------------------------------\n\n\n");
+				}
+			}	
+			//zerando carrinho
 			carrinho.clear();
-			
-			
-			System.out.print("Tabela Atualizada:\n\r");
+			//tabela atualizada apos a compra, estoque ajustado!
+			System.out.print("\nTabela Atualizada:\n\r");
 			insereBanner();
 			for (Produto item : lista) {		
 				System.out.println(item.getCodProduto()+"\t"+item.getPreco()+"\t\t"+item.getEstoque()+"\t\t"+item.getProduto());
 			}
-			System.out.println("Obrigado pela visita!!\n");
+			System.out.println("---------------------------------------------------------");
+			System.out.println("\nOBRIGADO PELA VISITA, ESPERAMOS TE VER DE NOVAMENTE!!\n");
 		}
 	}
 
