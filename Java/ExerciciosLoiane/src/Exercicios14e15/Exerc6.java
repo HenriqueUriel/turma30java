7b7286baf3ccc746a5644bd4994764d657022640
Henrique Uriel <riikilopes262@hotmail.comgit config --global user.name Henrique>
2021-08-18 21:10:05 -0300

    exercicio If Else

--- Java/ExerciciosLoiane/src/Exercicios14e15/Exerc6.java
+++ Java/ExerciciosLoiane/src/Exercicios14e15/Exerc6.java
@@ -15,11 +15,11 @@ public class Exerc6 {
 		System.out.print("Digite o terceiro numero: ");
 		num3 = ler.nextInt();
 
-		if (num1 > num2 && num1 > num3) {
+		if (num1 >= num2 && num1 >= num3) {
 			System.out.printf("O primeiro numero digitado (%d) é o maior",num1);
-		} else if (num2 > num1 && num2 > num3) {
+		} else if (num2 >= num1 && num2 >= num3) {
 			System.out.printf("O segundo numero digitado (%d) é o maior",num2);
-		} else if (num3 > num1 && num3 > num2) {
+		} else if (num3 >= num1 && num3 >= num2) {
 			System.out.printf("O terceiro numero digitado (%d) é o maior",num3);
 		}
 	}
