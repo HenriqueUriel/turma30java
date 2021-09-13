aa8e23b62d8c53deffb7f819c977cc4ac4551305
HenriqueUriel <riikilopes262@hotmail.com>
2021-09-13 16:24:09 -0300

    Adicionado link da tabela usuario com postagem

--- BlogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ BlogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -37,6 +37,10 @@ public class Postagem {
 	@ManyToOne
 	@JsonIgnoreProperties("postagem")
 	private Tema tema;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Usuario usuario;
 
 	public long getId() {
 		return id;
@@ -78,4 +82,11 @@ public class Postagem {
 		this.tema = tema;
 	}
 
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 }
--- BlogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
+++ BlogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/Usuario.java
@@ -1,12 +1,18 @@
 package org.generation.blogPessoal.model;
 
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "tb_usuario")
@@ -27,6 +33,10 @@ public class Usuario {
 	@NotNull
 	@Size(min=5,max=100)
 	private String senha;
+	
+	@OneToMany (mappedBy = "usuario", cascade = CascadeType.ALL)
+	@JsonIgnoreProperties ("usuario")
+	private List<Postagem> postagem;
 	
 	// Primeiro método Construtor(teste)
 	public Usuario(long id, String nome, String usuario, String senha) {
@@ -69,5 +79,11 @@ public class Usuario {
 
 	public void setSenha(String senha) {
 		this.senha = senha;
+	}
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
 	}
 }
