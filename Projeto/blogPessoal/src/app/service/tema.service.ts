40a4d6b99a330aa2eca15ae199cbb4db4d120722
HenriqueUriel <riikilopes262@hotmail.com>
2021-10-05 13:50:56 -0300

    put e delete de Tema criados

--- Projeto/blogPessoal/src/app/service/tema.service.ts
+++ Projeto/blogPessoal/src/app/service/tema.service.ts
@@ -19,7 +19,19 @@ export class TemaService {
     return this.http.get<Tema[]>('https://blogdouriel.herokuapp.com/tema', this.token)
   }
 
+  getByIdTema(id: number): Observable<Tema>{
+    return this.http.get<Tema>(`https://blogdouriel.herokuapp.com/tema/${id}`, this.token)
+  }
+
   postTema(tema: Tema): Observable<Tema> {
     return this.http.post<Tema>('https://blogdouriel.herokuapp.com/tema', tema, this.token)
+  }
+
+  putTema(tema: Tema): Observable<Tema>{
+    return this.http.put<Tema>('https://blogdouriel.herokuapp.com/tema', tema, this.token)
+  }
+
+  deleteTema(id: number) {
+    return this.http.delete(`https://blogdouriel.herokuapp.com/tema/${id}`, this.token)
   }
 }
