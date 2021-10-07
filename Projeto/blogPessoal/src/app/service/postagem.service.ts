a0c116d22296f46a34fee9e6e7853b410ff4216d
HenriqueUriel <riikilopes262@hotmail.com>
2021-10-07 00:42:20 -0300

    put e delete de postagens

--- Projeto/blogPessoal/src/app/service/postagem.service.ts
+++ Projeto/blogPessoal/src/app/service/postagem.service.ts
@@ -19,7 +19,19 @@ export class PostagemService {
     return this.http.get<Postagem[]>('https://blogdouriel.herokuapp.com/postagens', this.token)
   }
 
+  getByIdPostagem(id: number): Observable<Postagem>{
+    return this.http.get<Postagem>(`https://blogdouriel.herokuapp.com/postagens/${id}`, this.token)
+  }
+
   postPostagem(postagem: Postagem): Observable<Postagem>{
     return this.http.post<Postagem>('https://blogdouriel.herokuapp.com/postagens', postagem, this.token)
+  }
+
+  putPostagem(postagem: Postagem): Observable<Postagem>{
+    return this.http.put<Postagem>('https://blogdouriel.herokuapp.com/postagens', postagem, this.token)
+  }
+
+  deletePostagem(id: number){
+    return this.http.delete(`https://blogdouriel.herokuapp.com/postagens/${id}`, this.token)
   }
 }
