3c816cbc1b3715349f5c5030ef1d9cce1d82d5e9
HenriqueUriel <riikilopes262@hotmail.com>
2021-10-07 11:42:50 -0300

    put usuario adicionado

---
+++ Projeto/blogPessoal/src/app/edit/user-edit/user-edit.component.ts
@@ -0,0 +1,70 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from 'src/app/model/User';
+import { AuthService } from 'src/app/service/auth.service';
+import { environment } from 'src/environments/environment.prod';
+
+@Component({
+  selector: 'app-user-edit',
+  templateUrl: './user-edit.component.html',
+  styleUrls: ['./user-edit.component.css']
+})
+export class UserEditComponent implements OnInit {
+
+  user: User = new User()
+  idUser: number
+  confirmarSenha: string
+  tipoUsuario: string
+
+  constructor(
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit(){
+    window.scroll(0,0)
+
+    if(environment.token == ''){
+      this.router.navigate(['/entrar'])
+    }
+
+    this.idUser = this.route.snapshot.params['id']
+    this.findByIdUser(this.idUser)
+  }
+
+  confirmSenha(event: any) {
+    this.confirmarSenha = event.target.value
+  }
+
+  tipoUser(event: any){
+    this.tipoUsuario = event.target.value
+  }
+
+  atualizar(){
+    this.user.tipo = this.tipoUsuario
+
+    if(this.user.senha != this.confirmarSenha) {
+      alert('A senha esta incorreta.')
+    } else {
+      this.authService.atualizar(this.user).subscribe((resp: User) => {
+        this.user = resp
+        this.router.navigate(['/inicio'])
+        alert('Usuário atualizado com sucesso, faça o login novamente.')
+        environment.id = 0
+        environment.token = ''
+        environment.nome = ''
+        environment.foto = ''
+
+        this.router.navigate(['/entrar'])
+
+      })
+    }
+  }
+
+  findByIdUser(id: number){
+    this.authService.getByIdUser(id).subscribe((resp: User) =>{
+      this.user = resp
+    })
+  }
+}
--- Projeto/blogPessoal/src/app/service/auth.service.ts
+++ Projeto/blogPessoal/src/app/service/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -14,12 +14,23 @@ export class AuthService {
     private http: HttpClient
   ) { }
 
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+
   entrar(userLogin: UserLogin): Observable<UserLogin> {
     return this.http.post<UserLogin>('https://blogdouriel.herokuapp.com/usuarios/logar', userLogin)
   }
 
   cadastrar(user: User): Observable<User> {
     return this.http.post<User>('https://blogdouriel.herokuapp.com/usuarios/cadastrar', user)
+  }
+
+  atualizar(user: User): Observable<User>{
+    this.token = {
+      headers: new HttpHeaders().set('Authorization', environment.token)
+    }
+    return this.http.put<User>('https://blogdouriel.herokuapp.com/usuarios/atualizar', user, this.token)
   }
 
   getByIdUser(id: number): Observable<User>{
