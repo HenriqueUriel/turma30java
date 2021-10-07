74ddc1995ffc6b56f376106d6d7ef0b340fd9c87
HenriqueUriel <riikilopes262@hotmail.com>
2021-10-06 23:24:39 -0300

    implementação do post e get postagens

--- Projeto/blogPessoal/src/app/service/auth.service.ts
+++ Projeto/blogPessoal/src/app/service/auth.service.ts
@@ -22,6 +22,10 @@ export class AuthService {
     return this.http.post<User>('https://blogdouriel.herokuapp.com/usuarios/cadastrar', user)
   }
 
+  getByIdUser(id: number): Observable<User>{
+    return this.http.get<User>(`https://blogdouriel.herokuapp.com/usuarios/${id}`)
+  }
+
   logado() {
     let ok = false
 
